176237940497552842a8c181b03884e54f7cf360
Avocado <hamzayabdi@gmail.com>
2025-08-25 20:28:58 -0400

    option menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,14 +1,17 @@
 extends Control
 
+@onready var main_bussy: VBoxContainer = $"main bussy"
+@onready var opussy_panel: Panel = $"opussy panel"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	main_bussy.visible = true
+	opussy_panel.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pass 
 
 
 func _on_start_button_pressed() -> void:
@@ -17,7 +20,13 @@ func _on_start_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	print ("oppussy activated")
+	main_bussy.visible = false
+	opussy_panel.visible = true
 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
